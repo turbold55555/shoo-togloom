e633dff3712d6c328a1dae71104bab7225a44f8e
turuu <turuu@123.com>
2020-03-01 14:45:43 +0800

    shoog shiddeg bolgoh

---
+++ app.js
@@ -0,0 +1,30 @@
+// Тоглогчийн ээлжийг хадгалах хувьсагч, нэгдүгээр тоглогч 0, хоёрдугаар тоглогч 1 гэж тэмдэглэе.
+var activePlayer = 1;
+
+// Тоглогчдын цуглуулсан оноог хадгалах хувьсагч.
+var scores = [0, 0];
+
+//  Тоглогчийн ээлжиндээ цуглуулж байгаа оноог хадгалах хувьсагч.
+var roundScore = 0;
+
+
+//  Шооны аль талаараа буусныг хадгалах хувьсагч хэрэгтэй, 1-6 гэсэн утгыг энэ хувьсагчид санамсаргүйгээр үүсгэж өгнө.
+var diceNumber = Math.floor(Math.random()*6) + 1;
+
+document.getElementById("score-0").textContent = "0";
+document.getElementById("score-1").textContent = "0";
+document.getElementById("current-0").textContent = "0";
+document.getElementById("current-1").textContent = "0";
+
+var diceDom =  document.querySelector(".dice") ;
+// diceDom.style.display = "none";
+
+document.querySelector(".btn-roll").addEventListener("click", function (){
+    var diceNumber = Math.floor(Math.random()*6) + 1;
+
+    diceDom.style.display = "block";
+    diceDom.src = 'dice-' + diceNumber + '.png';
+ 
+});
+
+
